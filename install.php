5dfe85693b4192902328b8d507b4c64806d0d1c6
Alexander Walther <mail@alexplus.de>
2024-02-08 15:41:08 +0100

    Update install.php
    hotfix falscher Dateiname in install.php

--- install.php
+++ install.php
@@ -1,5 +1,5 @@
 <?php
 
 /* Tablesets aktualisieren */
-
-rex_yform_manager_table_api::importTablesets(rex_file::get(rex_path::addon($this->name, 'install/rex_product.tableset.json')));
+$addon = rex_addon::get('product');
+rex_yform_manager_table_api::importTablesets(rex_file::get(rex_path::addon($adonn->getName(), 'install/rex_product_tableset.json')));
