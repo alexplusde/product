42f1093a3cc20106a7c97719ba6c9c1232827467
Alexander Walther <github.com@alexplus.de>
2024-02-10 01:35:54 +0100

    Create compat.php

---
+++ lib/compat.php
@@ -0,0 +1,13 @@
+<?php
+
+class product_category extends alexplusde\product\product_category
+{
+}
+
+class product extends alexplusde\product\product
+{
+}
+
+class product_variant extends alexplusde\product\product_variant
+{
+}
