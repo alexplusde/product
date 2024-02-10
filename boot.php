f2f8cb5318157d1d782c3fd73721ddb0b0f69a16
Alexander Walther <mail@alexplus.de>
2024-02-10 19:50:33 +0100

    fix for editing pages

--- boot.php
+++ boot.php
@@ -24,20 +24,26 @@ rex_yform_manager_dataset::setModelClass(
 }
 
 \rex_extension::register('YFORM_DATA_LIST', static function ($ep) {
+    /** @var \rex_list $list */
+    $list = $ep->getSubject();
 
-    /* Wenn TableName mit 'rex_product' beginnt, dann */
-    if(strpos($ep->getParam('table')->getTableName(), 'rex_product') === 0) {
-        $list = $ep->getSubject();
+    if ('rex_product' == $ep->getParam('table')->getTableName()) {
 
         $list->setColumnFormat('id', 'custom', static function ($a) {
             $id = $a['value'];
-            return '<a href="' . \rex_url::backendPage('product/product_category', ['func' => 'edit', 'data_id' => $id]) . '"><i class="fa fa-edit"></i> ' . $id . '</a>';
+            
+            $_csrf_key = \rex_yform_manager_table::get('rex_product')->getCSRFKey();
+            $token = \rex_csrf_token::factory($_csrf_key)->getUrlParams();
+
+            $params = [];
+            $params['table_name'] = 'rex_product';
+            $params['rex_yform_manager_popup'] = '0';
+            $params['_csrf_token'] = $token['_csrf_token'];
+            $params['data_id'] = $a['list']->getValue('id');
+            $params['func'] = 'edit';
+
+            return '<a href="' . \rex_url::backendPage('product/product', $params) . '"><i class="fa fa-edit"></i> ' . $id . '</a>';
         });
-    }
-
-
-    if ('rex_product' == $ep->getParam('table')->getTableName()) {
-        $list = $ep->getSubject();
 
         $list->setColumnFormat(
             'name',
@@ -59,7 +65,22 @@ rex_yform_manager_dataset::setModelClass(
         );
     }
     if ('rex_product_category' == $ep->getParam('table')->getTableName()) {
-        /** @var \rex_list $list */
+
+        $list->setColumnFormat('id', 'custom', static function ($a) {
+            $id = $a['value'];
+            
+            $_csrf_key = \rex_yform_manager_table::get('rex_product_category')->getCSRFKey();
+            $token = \rex_csrf_token::factory($_csrf_key)->getUrlParams();
+
+            $params = [];
+            $params['table_name'] = 'rex_product_category';
+            $params['rex_yform_manager_popup'] = '0';
+            $params['_csrf_token'] = $token['_csrf_token'];
+            $params['data_id'] = $a['list']->getValue('id');
+            $params['func'] = 'edit';
+
+            return '<a href="' . \rex_url::backendPage('product/category', $params) . '"><i class="fa fa-edit"></i> ' . $id . '</a>';
+        });
 
         $list->setColumnFormat(
             'name',
@@ -76,13 +97,28 @@ rex_yform_manager_dataset::setModelClass(
                 $params['data_id'] = $a['list']->getValue('id');
                 $params['func'] = 'edit';
 
-                return '<a href="' . \rex_url::backendPage('product/product_category', $params) . '">' . $a['value'] . '</a>';
+                return '<a href="' . \rex_url::backendPage('product/category', $params) . '">' . $a['value'] . '</a>';
             },
         );
     }
     if ('rex_product_variant' == $ep->getParam('table')->getTableName()) {
-        $list = $ep->getSubject();
+        $list->setColumnFormat('id', 'custom', static function ($a) {
+            $id = $a['value'];
+            
+            $_csrf_key = \rex_yform_manager_table::get('rex_product_variant')->getCSRFKey();
+            $token = \rex_csrf_token::factory($_csrf_key)->getUrlParams();
 
+            $params = [];
+            $params['table_name'] = 'rex_product_variant';
+            $params['rex_yform_manager_popup'] = '0';
+            $params['_csrf_token'] = $token['_csrf_token'];
+            $params['data_id'] = $a['list']->getValue('id');
+            $params['func'] = 'edit';
+
+            return '<a href="' . \rex_url::backendPage('product/variant', $params) . '"><i class="fa fa-edit"></i> ' . $id . '</a>';
+        });
+
+        
         $list->setColumnFormat(
             'name',
             'custom', 
@@ -98,7 +134,7 @@ rex_yform_manager_dataset::setModelClass(
                 $params['data_id'] = $a['list']->getValue('id');
                 $params['func'] = 'edit';
 
-                return '<a href="' . \rex_url::backendPage('product/product_variant', $params) . '">' . $a['value'] . '</a>';
+                return '<a href="' . \rex_url::backendPage('product/variant', $params) . '">' . $a['value'] . '</a>';
             },
         );
     }
