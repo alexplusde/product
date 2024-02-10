987a335c6ef63dca99451f25deb69a020de44a74
Alexander Walther <mail@alexplus.de>
2024-02-10 21:23:15 +0100

    fix installer update failure

--- install.php
+++ install.php
@@ -15,12 +15,12 @@ $addon = rex_addon::get('product');
 
 /* Tablesets aktualisieren */
 if (rex_addon::get('yform') && rex_addon::get('yform')->isAvailable()) {
-    rex_yform_manager_table_api::importTablesets(rex_file::get(rex_path::addon(rex_addon::get('product')->getName(), 'install/rex_product_tableset.json')));
+    rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ .  'install/rex_product_tableset.json'));
 }
 
 if (!rex_media::get('product_fallback_image.png')) {
 
-    rex_file::copy(rex_path::addon('product', '/install/product_fallback_image.png'), rex_path::media('product_fallback_image.png'));
+    rex_file::copy(__DIR__ . '/install/product_fallback_image.png', rex_path::media('product_fallback_image.png'));
     $data = [];
     $data['title'] = 'Aktuelles - Fallback-Image';
     $data['category_id'] = 0;
@@ -39,12 +39,12 @@ if (rex_addon::get('url') && rex_addon::get('url')->isAvailable()) {
 
         $rex_product_category = array_filter(rex_sql::factory()->getArray("SELECT * FROM rex_url_generator_profile WHERE `table_name` = '1_xxx_rex_product_category'"));
         if (!$rex_product_category) {
-            $query = rex_file::get(rex_path::addon('product', 'install/rex_url_profile_product_category.sql'));
+            $query = rex_file::get(__DIR__ .  'install/rex_url_profile_product_category.sql');
             rex_sql::factory()->setQuery($query);
         }
         $rex_product = array_filter(rex_sql::factory()->getArray("SELECT * FROM rex_url_generator_profile WHERE `table_name` = '1_xxx_rex_product'"));
         if (!$rex_product) {
-            $query = rex_file::get(rex_path::addon('product', 'install/rex_url_profile_product.sql'));
+            $query = rex_file::get(__DIR__ . 'install/rex_url_profile_product.sql');
             rex_sql::factory()->setQuery($query);
         }
         /* URL-Profile wurden bereits einmal installiert, daher nicht nochmals installieren und Entwickler-Einstellungen respektieren */
